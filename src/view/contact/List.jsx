d0c6cdb30bd1ad92aeb40a8e808896f68ad96160
alldynovryaldy <alldy1400018051@webmail.uad.ac.id>
2022-04-04 09:31:20 +0700

    use axios to get data

--- src/view/contact/List.jsx
+++ src/view/contact/List.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "../../axios";
 import ContactList from "../component/ContactList";
 
 const List = () => {
-   const [contacts, setContacts] = useState([
-      { id: 1, name: "Alldy", number: "082138534673" },
-      { id: 2, name: "Novryaldy", number: "082222222" },
-      { id: 3, name: "empat", number: "082222222" },
-      { id: 4, name: "Lima", number: "082222222" },
-   ]);
+   // diganti dengan json server
+   // const [contacts, setContacts] = useState([
+   //    { id: 1, name: "Alldy", number: "082138534673" },
+   //    { id: 2, name: "Novryaldy", number: "082222222" },
+   //    { id: 3, name: "empat", number: "082222222" },
+   //    { id: 4, name: "Lima", number: "082222222" },
+   // ]);
+
+   const [contacts, setContacts] = useState([]);
 
    // init handleDelet
    const handleDelete = (idContact) => {
@@ -20,6 +24,19 @@ const List = () => {
 
       // note : data akan reset jika halaman di refresh, karena data hanya di simpan dalam variable objek, bukan database
    };
+
+   // function akan di jalankan ketika halaman di render
+   useEffect(() => {
+      axios
+         .get("/contact")
+         .then((res) => {
+            // console.log(res);
+            setContacts(res.data);
+         })
+         .catch((e) => {
+            console.log(e);
+         });
+   }, []);
 
    // parsing state contact & event handler ke Child component yaitu contactlist.jsx
    return <ContactList contacts={contacts} handleDelete={handleDelete} />;
