4a078b4c8ac428996eb4f0e8fd8d4efa5f18f0cd
alldynovryaldy <alldy1400018051@webmail.uad.ac.id>
2022-04-04 10:13:58 +0700

    loading & error response API

--- src/view/contact/List.jsx
+++ src/view/contact/List.jsx
@@ -11,7 +11,14 @@ const List = () => {
    //    { id: 4, name: "Lima", number: "082222222" },
    // ]);
 
+   // state contact
    const [contacts, setContacts] = useState([]);
+
+   // state error
+   const [error, setError] = useState("");
+
+   // state loading di set true
+   const [loading, setLoading] = useState(true);
 
    // init handleDelet
    const handleDelete = (idContact) => {
@@ -30,16 +37,26 @@ const List = () => {
       axios
          .get("/contact")
          .then((res) => {
-            // console.log(res);
             setContacts(res.data);
          })
          .catch((e) => {
-            console.log(e);
+            setError(e.message);
+         })
+         // jika axios sudh selesai di jalankan
+         .finally(() => {
+            // loading false saat data belum di temukan
+            setLoading(false);
          });
    }, []);
 
    // parsing state contact & event handler ke Child component yaitu contactlist.jsx
-   return <ContactList contacts={contacts} handleDelete={handleDelete} />;
+   return (
+      <div>
+         {error ?? { error }}
+         {loading ? "Loading..." : ""}
+         <ContactList contacts={contacts} handleDelete={handleDelete} />
+      </div>
+   );
 };
 
 export default List;
